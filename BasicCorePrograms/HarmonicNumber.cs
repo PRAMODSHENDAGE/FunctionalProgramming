533f3adc87a8d8ad3153a7a20c0622041d9e083b
KajalShendage <shendagekajal123@gmail.com>
2022-08-26 23:59:46 +0530

    [Pramod] Add: Harmonic Number

---
+++ BasicCorePrograms/HarmonicNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class HarmonicNumber
+    {
+        public static void HarmonicNumberMmethod()
+        {
+            Console.WriteLine("Enter the n value for Harmonic Number Series");
+            int n = int.Parse(Console.ReadLine());
+            if (n != 0)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.Write("1/{0} + ", i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entered Zero number and you need to enter Non Zero number");
+            }
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Welcome to basic core program");
 while (true)
 {
-    Console.WriteLine("\nSelect program\n 1.Flip Coin\n 2.Leap Year");
+    Console.WriteLine("\nSelect program\n 1.Flip Coin\n 2.Leap Year\n 3.Power of 2\n 4.Harmonic Number");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
@@ -13,6 +13,9 @@ while (true)
             LeapYear.Yearmethod();
             break;
         case 3:
+            PowerOf2.PowerOf2Method();
+            break;
+        case 4:
             PowerOf2.PowerOf2Method();
             break;
         default:
