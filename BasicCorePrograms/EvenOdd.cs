7fe5dca6624b16cc0dde7c79cfba4e94f6c64c90
KajalShendage <shendagekajal123@gmail.com>
2022-08-27 00:27:12 +0530

    [Pramod] Add: Even or Odd

---
+++ BasicCorePrograms/EvenOdd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class EvenOdd
+    {
+        public static void EvenOrOdd()
+        {
+            int i;
+            Console.Write("\nEnter a Number : ");
+            i = int.Parse(Console.ReadLine());
+            if (i % 2 == 0)
+            {
+                Console.Write("Entered Number is an Even Number");
+            }
+            else
+            {
+                Console.Write("Entered Number is an Odd Number");
+            }
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Welcome to basic core program");
 while (true)
 {
-    Console.WriteLine("\nSelect program\n 1.Flip Coin\n 2.Leap Year\n 3.Power of 2\n 4.Harmonic Number\n 5.Factors\n 6.QuotientRemainder\n 7.Swap Numbers");
+    Console.WriteLine("\nSelect program\n 1.Flip Coin\n 2.Leap Year\n 3.Power of 2\n 4.Harmonic Number\n 5.Factors\n 6.QuotientRemainder\n 7.Swap Numbers\n 8.Even or Odd");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
@@ -26,6 +26,9 @@ while (true)
             break;
         case 7:
             SwapNumbers.SwapMethod();
+            break;
+        case 8:
+            EvenOdd.EvenOrOdd();
             break;
         default:
             Console.WriteLine("Select correct option");
