28aa0e88d2abd487d2f6d312507f6ec4e6cfe719
KajalShendage <shendagekajal123@gmail.com>
2022-08-26 23:43:39 +0530

    [Pramod] Add: Leap Year

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -2,12 +2,15 @@
 Console.WriteLine("Welcome to basic core program");
 while (true)
 {
-    Console.WriteLine("\nSelect program\n 1.Flip Coin");
+    Console.WriteLine("\nSelect program\n 1.Flip Coin\n 2.Leap Year");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
         case 1:
             FlipCoin.FlipCoinPercentage();
+            break;
+        case 2:
+            LeapYear.Yearmethod();
             break;
         default:
             Console.WriteLine("Select correct option");
