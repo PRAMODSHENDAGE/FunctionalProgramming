b9158fcf90a8b63a56f5d10a25ab812707323d52
KajalShendage <shendagekajal123@gmail.com>
2022-08-26 23:39:30 +0530

    [Pramod] Add: Flip Coin

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BasicCorePrograms;
+Console.WriteLine("Welcome to basic core program");
+while (true)
+{
+    Console.WriteLine("\nSelect program\n 1.Flip Coin");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            FlipCoin.FlipCoinPercentage();
+            break;
+        default:
+            Console.WriteLine("Select correct option");
+            break;
+    }
+}
+
